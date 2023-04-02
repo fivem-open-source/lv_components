36813dc61fa59e0ef1d751e10691242b16b7bfd8
Luca <weixlerluca@icloud.com>
2023-04-03 00:37:46 +0200

    uploaded files

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'Lvcq'
+description 'Hides the default GTA5 vehicle name, street name and money display'
+version '1.0'
+
+client_script 'client.lua'
